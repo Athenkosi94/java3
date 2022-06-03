093d486ee902a71722262fd5aea6faf04eaabcc5
user <athimbityana@gmail com>
2022-06-03 13:42:34 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1 = 90;
+        double num2 = 90.77;
+        String str1 = " I like Automation";
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(str1);
     }
 }
